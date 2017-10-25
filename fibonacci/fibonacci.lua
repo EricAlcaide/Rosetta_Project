e7375adf7d69dc896cfab43cf005bbe8ed73803a
TeoTech2 <tfmollano@gmail.com>
2017-10-25 19:57:20 -0400

    Update fibonacci.lua

--- fibonacci/fibonacci.lua
+++ fibonacci/fibonacci.lua
@@ -1,13 +1,13 @@
-fibonacci_to_perform = 10
+fibonacci_to_perform = 11
 iter = 2
 fibs = {0,1}
 
 function fibonacci()
 
     while iter<=fibonacci_to_perform do
-    	print(fibs[iter])
     	iter = iter + 1
     	fibs[iter] = fibs[iter-1] + fibs[iter-2]
+        print(fibs[iter])
     	
     end
     
