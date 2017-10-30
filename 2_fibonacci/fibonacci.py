a607e62d33f3e5278a56801d8a03bc6989730bd2
Vedarth Sharma <vedarth.sharma@gmail.com>
2017-10-30 09:28:08 +0530

    Update fibonacci.py
    Improved the solution by adding a feature to print n fibonacci numbers.

--- 2_fibonacci/fibonacci.py
+++ 2_fibonacci/fibonacci.py
@@ -2,14 +2,16 @@
 # Python implementation of the Fibonacci Series
 # Author: Eric Alcaide
 
-def fibonacci():
+def fibonacci(n):
     prev, actual = 0, 1
-    
-    for q in range(10):
+    if(n>0):
+        print("0")
+    for q in range(n):
         actual, prev = actual+prev, actual
         print(actual)
 
 def main():
+    n = input("Enter the number of terms of fibonacci numbers")
     fibonacci()
 
 if __name__ == "__main__":
