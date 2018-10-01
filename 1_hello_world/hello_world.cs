d5ef4e24c80968f80dab01f18284d97f292d4903
Joe <Josephharrisonlim@gmail.com>
2018-10-01 16:01:09 -0400

    add csharp hello_world

---
+++ 1_hello_world/hello_world.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World.");
+            String hw = "Hello World";
+            Console.WriteLine(hw);
+        }
+    }
+}
