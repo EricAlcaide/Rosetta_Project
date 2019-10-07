c677c6e5c9e58d381ba499e4ab43aeb7372c4047
JonDev17 <43909343+JonDev17@users.noreply.github.com>
2019-10-07 19:40:39 +0200

    Add files via upload

---
+++ 1_hello_world/hello_world.hs
@@ -0,0 +1,7 @@
+main = do
+  --This line prints the String "Hello, World."
+  putStr "Hello, World."
+  --Define a variable within this do-block.
+  let myVar = "Hello World"
+  --Print the contents of the variable on the screen.
+  putStr myVar
