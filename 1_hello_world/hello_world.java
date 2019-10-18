f9e7578b22494aa08eabc0cc84e46c352dbc6700
Subhradeep Saha <46227193+SubhradeepSS@users.noreply.github.com>
2019-10-18 23:14:39 +0530

    Update hello_world.java

--- 1_hello_world/hello_world.java
+++ 1_hello_world/hello_world.java
@@ -5,7 +5,7 @@ public class hello_world {
 
   public static void greeting(String str) {
     // This line of code prints the first line of output
-    System.out.print("Hello, World.");
+    System.out.println("Hello, World.");
     // Second line of output - the contents of 'word' here.
     System.out.println(str);
   }
