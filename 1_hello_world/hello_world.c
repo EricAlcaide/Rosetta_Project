487915d37ece9cd7b9b779fe35ff455b8ca90410
Aryan Kharbanda <kharbanda.aryan00@gmail.com>
2019-10-23 01:48:09 +0530

    updated codes

--- 1_hello_world/hello_world.c
+++ 1_hello_world/hello_world.c
@@ -1,16 +1,9 @@
-/* Hello world implementation for Rosetta Stone. */
+// Hello world implementation for Rosetta Stone.
 
 #include <stdio.h>
 
-void hello(char* word)
+int main()
 {
     printf("Hello, World.\n");
-    printf("%s\n", word);
-}
-
-int main(void)
-{
-    char* word = "Hello, World.";
-    hello(word);
     return 0;
 }
